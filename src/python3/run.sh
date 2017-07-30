7f0de8cd2856d4a902a979b6e6832a8bc0c8a656
transferXiang <448217518@qq.com>
2017-07-30 19:38:51 +0800

    更新python3Dockerfile

--- src/python3/run.sh
+++ src/python3/run.sh
@@ -1,2 +1,31 @@
-cp ../../resource/sources.list ./
-sudo docker build -t python3:latest ./
+#!/bin/bash
+
+Image=python3:latest
+Container=python3
+Volume=/media/sf_e
+
+case $1 in
+    build)
+        echo "build $Image"
+        sudo docker build -t $Image ./
+    ;;
+    create)
+        echo "create: name[$Container]"
+        sudo docker run -it -v $Volume:/src --name $Container $Image
+    ;;
+     start)
+         echo "start $Container"
+         sudo docker start $Container
+     ;;
+     stop)
+         echo "stop $Container"
+         sudo docker stop $Container
+     ;;
+     remove)
+         echo "remove: name[$Container]"
+         sudo docker rm $Container
+     ;;
+     *)
+         echo "usage:build|create|start|stop|remove"
+     ;;
+ esac
